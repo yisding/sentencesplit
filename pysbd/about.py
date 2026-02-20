d11f705a2b64433cdd2c78a2d377f6ff16a8acc6
Yi Ding <yi.s.ding@gmail.com>
2026-02-19 21:59:48 -0800

    review comments

--- pysbd/about.py
+++ pysbd/about.py
@@ -1,10 +1,13 @@
 # inspired from:
 # https://python-packaging-user-guide.readthedocs.org/en/latest/single_source_version/
 
-from importlib.metadata import version
+from importlib.metadata import PackageNotFoundError, version
 
 __title__ = "pysbd"
-__version__ = version("pysbd")
+try:
+    __version__ = version("pysbd")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
 __summary__ = "pysbd (Python Sentence Boundary Disambiguation) is a rule-based sentence boundary detection that works out-of-the-box across many languages."
 __uri__ = "http://nipunsadvilkar.github.io/"
 __author__ = "Nipun Sadvilkar"
